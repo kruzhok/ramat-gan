73c1442d9101611c6cfee90c02414a067841d9b1
pa7lux <pa7lux@gmail.com>
2023-04-03 01:23:09 +0300

    article draft

---
+++ scripts/menu.js
@@ -0,0 +1,13 @@
+const openBtn = document.querySelector('.menu-button--open');
+const closeBtn = document.querySelector('.menu-button--close');
+const menu = document.querySelector('.header-menu');
+
+openBtn.onclick = () => {
+  menu.classList.add('opened');
+  document.body.style.overflow = 'hidden';
+}
+
+closeBtn.onclick = () => {
+  menu.classList.remove('opened');
+  document.body.style.overflow = 'auto';
+}
