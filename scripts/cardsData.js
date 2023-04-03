9bf3b5bbfa5a76fbb16efbbe636e39a8ac0a8b23
pa7lux <pa7lux@gmail.com>
2023-04-04 02:25:56 +0300

    slider

---
+++ scripts/cardsData.js
@@ -0,0 +1,50 @@
+const dataArray = [
+  {
+    id: 'support',
+    theme: 'darkgreen',
+    coverUrl: '/images/covers-svg/support.svg',
+    coverText: 'Текст под саппортом',
+    content: [
+      {
+        text: 'Привет',
+        image: ''
+      },
+      {
+        text: 'Shalom',
+        image: ''
+      }
+    ]
+  },
+  {
+    id: 'friends',
+    theme: 'lightpurple',
+    coverUrl: '/images/covers-svg/how-to-find-friends.svg',
+    coverText: 'История о том, как Сёма была в полиции и узнала много нового',
+    content: [
+      {
+        text: 'Привет',
+        image: ''
+      },
+      {
+        text: 'Shalom',
+        image: ''
+      }
+    ]
+  },
+  {
+    id: 'help',
+    theme: 'yellow',
+    coverUrl: '/images/covers-svg/how-to-find-friends.svg',
+    coverText: 'История о том, как Сёма была в полиции и узнала много нового',
+    content: [
+      {
+        text: 'Привет',
+        image: ''
+      },
+      {
+        text: 'Shalom',
+        image: ''
+      }
+    ]
+  }
+]
--- scripts/swiper-init.js
+++ scripts/swiper-init.js
@@ -1,5 +1,6 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
+  direction: 'horizontal',
   loop: true,
 
   // If we need pagination
