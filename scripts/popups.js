7602eb7a4eb7a9ab866a843fb1e15ad34f7d9d4a
pa7lux <pa7lux@gmail.com>
2023-04-04 10:30:35 +0300

    popups

--- scripts/popups.js
+++ scripts/popups.js
@@ -7,11 +7,13 @@ const dataArray = [
     content: [
       {
         text: 'Привет',
-        image: ''
+        image: '',
+        author: 'Петя'
       },
       {
         text: 'Shalom',
-        image: ''
+        image: '',
+        author: 'Вася'
       }
     ]
   },
@@ -23,11 +25,13 @@ const dataArray = [
     content: [
       {
         text: 'Привет',
-        image: ''
+        image: '',
+        author: 'Петя'
       },
       {
         text: 'Shalom',
-        image: ''
+        image: '',
+        author: 'Вася'
       }
     ]
   },
@@ -39,11 +43,13 @@ const dataArray = [
     content: [
       {
         text: 'Привет',
-        image: ''
+        image: '',
+        author: 'Петя'
       },
       {
         text: 'Shalom',
-        image: ''
+        image: '',
+        author: 'Вася'
       }
     ]
   }
@@ -52,43 +58,35 @@ const dataArray = [
 
 
 
-const popup = document.querySelector('.cards-popup');
+
 const cards = document.querySelectorAll('.clickable-card');
+const popupTmp = document.querySelector('.slider-popup-template');
 const slideTemplate = document.querySelector('.card-slide-tmp');
-const slider = document.querySelector('.cards-popup-slider .swiper-wrapper');
 
 let isOpened = false;
 
 cards.forEach((item) => {
+  let popup;
   item.onclick = () => {
-    popup.classList.add('is-opened');
     document.body.style.overflow = 'hidden';
-    isOpened = true;
     const dataForClickedCard = findWithId(dataArray, item.id)
-    console.log(dataForClickedCard);
-    document.querySelector('.cards-popup-slider').classList.add(`theme-${dataForClickedCard.theme}`)
-    document.querySelector('.card-slide-illustration').src = dataForClickedCard.coverUrl;
-    document.querySelector('.card-slide-cover-text').textContent = dataForClickedCard.coverText;
-    dataForClickedCard.content.forEach((obj) => {
-      generateSlide(slideTemplate, obj, slider);
+    generateSlider(popupTmp, dataForClickedCard, document.body)
+    popup = document.querySelector('.cards-popup');
+    dataForClickedCard.content.forEach(item => {
+      generateSlide(slideTemplate, item, popup.querySelector('.swiper-wrapper'));
     })
+    
+    popup.onclick = (e) => {
+      if(e.target.contains(document.querySelector('.cards-popup-slider'))) {
+        document.body.style.overflow = 'auto';
+        popup.remove();
+      }
+    }
+    initializeSwiper()
   }
 })
 
-popup.onclick = (e) => {
-  if(e.target.contains(document.querySelector('.cards-popup-slider'))) {
-    popup.classList.remove('is-opened');
-    document.querySelector('.cards-popup-slider').className = 'cards-popup-slider swiper';
-    popup.querySelectorAll('.swiper-slide').forEach(item => {
-      if(![...item.classList].includes('card-slide-cover')) {
-        item.remove();
-      }
-    })
-    document.body.style.overflow = 'auto';
-    isOpened = false;
 
-  }
-}
 
 function findWithId(data, id) {
   const item = data.find((el) => {
@@ -97,10 +95,43 @@ function findWithId(data, id) {
   return item;
 }
 
+function generateSlider(tmp, obj, parent) {
+  const node = tmp.content.cloneNode(true);
+  node.querySelector('.card-slide-illustration').src = obj.coverUrl;
+  node.querySelector('.cards-popup-slider').classList.add(`theme-${obj.theme}`)
+  node.querySelector('.card-slide-cover-text').textContent = obj.coverText;
+  parent.append(node);
+}
+
 function generateSlide(tmp, obj, slider) {
   const node = tmp.content.cloneNode(true);
   node.querySelector('.card-slide-content').textContent = obj.text;
+  node.querySelector('.card-slide-author').textContent = obj.author;
   slider.append(node);
+}
+
+function initializeSwiper() {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 }
 
 
