0524d1ecdb66b63305cf3b2cbe56f174e70d4fef
Sasha Patlukh <pa7lux@gmail.com>
2023-07-02 23:22:23 +0400

    gradient on body

---
+++ scripts/gradient-drawer.js
@@ -0,0 +1,20 @@
+const body = document.querySelector('.index');
+const fixedSection = document.querySelector('.lost')
+
+function drawBack(el, source) {
+  const color = getComputedStyle(source).backgroundColor;
+  const height = getComputedStyle(source).height;
+  const finalColor = getComputedStyle(el).backgroundColor
+  el.style.backgroundImage = `linear-gradient(to top, ${color} ${height}, ${finalColor} ${height} 100%)`;
+}
+
+setTimeout(()=>{
+  drawBack(body, fixedSection)
+}, 50)
+
+
+window.onresize = () => {
+  setTimeout(()=>{
+    drawBack(body, fixedSection)
+  }, 50)
+};
