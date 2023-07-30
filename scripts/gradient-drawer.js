f0d6fbd69d7b3dbb2c6f09d057f1503fe4666be8
Sasha Patlukh <pa7lux@gmail.com>
2023-07-30 22:47:12 +0400

    Сделал полосы и подложку в life.html

--- scripts/gradient-drawer.js
+++ scripts/gradient-drawer.js
@@ -4,13 +4,14 @@ const fixedSection = document.querySelector('.lost')
 function drawBack(el, source) {
   const color = getComputedStyle(source).backgroundColor;
   const height = getComputedStyle(source).height;
+  console.log(height)
   const finalColor = getComputedStyle(el).backgroundColor
   el.style.backgroundImage = `linear-gradient(to top, ${color} ${height}, ${finalColor} ${height} 100%)`;
 }
 
 setTimeout(()=>{
   drawBack(body, fixedSection)
-}, 50)
+}, 100)
 
 
 window.onresize = () => {
