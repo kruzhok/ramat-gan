a4d0a955d7b18e28345e6393fdf4c0f690ce5ba1
Sasha Patlukh <pa7lux@gmail.com>
2023-08-01 20:50:39 +0400

    фиксит появление горизонтального скролла

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -5,10 +5,10 @@ const menu = document.querySelector('.header-menu');
 openBtn.onclick = () => {
   menu.style.transition = 'transform 1s';
   menu.classList.add('opened');
-  document.body.style.overflow = 'hidden';
+  document.body.style.overflowY = 'hidden';
 };
 
 closeBtn.onclick = () => {
   menu.classList.remove('opened');
-  document.body.style.overflow = 'auto';
+  document.body.style.overflowY = 'auto';
 };
