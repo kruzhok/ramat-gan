17f8fcb6bbecbf57f443b5b20b4e9a412db83a51
Sasha Patlukh <pa7lux@gmail.com>
2023-08-02 00:29:53 +0400

    fixes

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -6,9 +6,11 @@ openBtn.onclick = () => {
   menu.style.transition = 'transform 1s';
   menu.classList.add('opened');
   document.body.style.overflow = 'hidden';
+  document.querySelector('html').style.overflow = 'hidden';
 };
 
 closeBtn.onclick = () => {
   menu.classList.remove('opened');
   document.body.style.overflow = 'auto';
+  document.querySelector('html').style.overflow = 'auto';
 };
