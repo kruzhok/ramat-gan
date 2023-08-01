12a019863c1141f21286221e6e8fcf80338a870b
Sasha Patlukh <pa7lux@gmail.com>
2023-08-02 00:02:25 +0400

    маленькие хаки для сафари

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -5,10 +5,10 @@ const menu = document.querySelector('.header-menu');
 openBtn.onclick = () => {
   menu.style.transition = 'transform 1s';
   menu.classList.add('opened');
-  document.body.style.overflowY = 'hidden';
+  document.body.style.overflow = 'hidden';
 };
 
 closeBtn.onclick = () => {
   menu.classList.remove('opened');
-  document.body.style.overflowY = 'auto';
+  document.body.style.overflow = 'auto';
 };
